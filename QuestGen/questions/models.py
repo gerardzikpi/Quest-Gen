dfa7a808b7a173e90ff9aa9dd1129f46ee7b9ee3
gerardzikpi <kodinapetra@yahoo.com>
2026-06-13 15:20:35 -0700

    changed courses model to course

--- QuestGen/questions/models.py
+++ QuestGen/questions/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Courses(models.Model):
+class Course(models.Model):
     name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
 
 class Topic(models.Model):
-    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
+    
 
     def __str__(self):
         return f"{self.course.name} - {self.name}"
