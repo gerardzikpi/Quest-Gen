acd70bc767767fe5e30b3c7dfab7b28a7c29f758
gerardzikpi <kodinapetra@yahoo.com>
2026-06-13 15:21:51 -0700

    configured the admin database

--- QuestGen/questions/admin.py
+++ QuestGen/questions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course, Topic, Question
 
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Topic)
+admin.site.register(Question)
