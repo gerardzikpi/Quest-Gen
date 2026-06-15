185cc003dc1e81514f04f94a4fdb88fcc9bb3ff7
gerardzikpi <kodinapetra@yahoo.com>
2026-06-14 18:06:17 -0700

    test build fix 7

--- QuestGen/QuestGen/wsgi.py
+++ QuestGen/QuestGen/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuestGen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuestGen.QuestGen.settings')
 application = get_wsgi_application()
 application = WhiteNoise(application)
 
