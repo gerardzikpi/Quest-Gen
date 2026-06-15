136b132db517120f1e0bb13e7b131ab1d5480cea
gerardzikpi <kodinapetra@yahoo.com>
2026-06-14 18:25:26 -0700

    reversed test build 7

--- QuestGen/QuestGen/settings.py
+++ QuestGen/QuestGen/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
-    'QuestGen.questions',
-    'QuestGen.theme',
+    'questions',
+    'theme',
 ]
 
 MIDDLEWARE = [
